ddb9748476670c2595077b4762ecc82a9e23896c
Gary Bains <gsb.gurpreet@gmail.com>
2017-11-15 16:20:51 -0500

    Get basic JS working

--- server/public/javascripts/index.js
+++ server/public/javascripts/index.js
@@ -1,1 +1,17 @@
-console.log('Hello');
+var imgUrl = "";
+
+$( document ).ready(function() {
+
+    $('#getImgBtn').on('click', function() {
+        console.log('#getImgBtn');
+
+        imgUrl = $('#imgUrlIp').val();
+        $("#img").attr("src", imgUrl);
+    });
+
+    $('#anaImgBtn').on('click', function() {
+        console.log('#anaImgBtn');
+        // Do your AJAX call to ML API here
+    });
+
+});
